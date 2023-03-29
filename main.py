c4d666700a7189795274753a6b8daaa6d485f7a3
tortik <kirtaev2008@mail.ru>
2023-03-30 01:26:37 +0900

    first day создал часть основных ктопок

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import telebot
+from telebot import types
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+bot = telebot.TeleBot('6197994890:AAH4gv_1nrvzAGGzQm1OxYKsXumDH9P8FvI')
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    #print(message.from_user.id)
+    markup = types.ReplyKeyboardMarkup()
+    if message.from_user.id in m.keys():
+        # Создаем клавиатуру с кнопкой "Расписание"
+
+        itembtn = types.KeyboardButton('Расписание')
+        markup.add(itembtn)
+        news = types.KeyboardButton('Важные новости')
+        markup.add(news)
+
+        # Отправляем приветственное сообщение с клавиатурой
+        if message.from_user.id == 1370770852:
+            bot.reply_to(message, "Приветствую мой господин", reply_markup=markup)
+        else:
+            bot.reply_to(message, "Приветствую! Это чат для урегулирования расписания занятий в ДНОЦ (CERC)", reply_markup=markup)
+    else:
+        bot.reply_to(message, "Увы, но у тебя нет доступа :)", reply_markup=markup)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Обработка нажатия кнопки "Расписание"
+@bot.message_handler(func=lambda message: message.text == 'Расписание')
+def send_schedule(message):
+    markup = types.ReplyKeyboardMarkup()
+    # Отправляем расписание
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    itembtn = types.KeyboardButton('Эта неделя')
+    markup.add(itembtn)
+    news = types.KeyboardButton('Следующая неделя')
+    markup.add(news)
+    bot.reply_to(message, "в разработке", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Важные новости')
+def send_schedule(message):
+    bot.reply_to(message, "в разработке")
+
+
+@bot.message_handler(func=lambda message: message.text == 'Эта неделя')
+def send_schedule(message):
+    markup = types.ReplyKeyboardMarkup()
+    # Отправляем расписание
+
+    d1 = types.KeyboardButton('Понедельник')
+    markup.add(d1)
+    d2 = types.KeyboardButton('Вторник')
+    markup.add(d2)
+    d3 = types.KeyboardButton('Среда')
+    markup.add(d3)
+    d4 = types.KeyboardButton('Четверг')
+    markup.add(d4)
+    d5 = types.KeyboardButton('Пятьница')
+    markup.add(d5)
+    d6 = types.KeyboardButton('Суббота')
+    markup.add(d6)
+    d7 = types.KeyboardButton('Воскресенье')
+    markup.add(d7)
+    bot.reply_to(message, "в разработке", reply_markup=markup)
+
+m = {1370770852: ['Разработчик', 0]}
+bot.polling()
